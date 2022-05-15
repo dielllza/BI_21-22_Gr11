79609cc2391fd08320e0033d8165d417ba291683
Diellza Aliu <diellza.aliu213@gmail.com>
2022-05-15 02:03:40 +0200

    add sql script

---
+++ mysql_connection/app_creator.sql
@@ -0,0 +1,52 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema app_creator
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema app_creator
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `app_creator` DEFAULT CHARACTER SET utf8 ;
+USE `app_creator` ;
+
+-- -----------------------------------------------------
+-- Table `app_creator`.`roles`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `app_creator`.`roles` (
+  `id` INT NOT NULL,
+  `role` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `app_creator`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `app_creator`.`users` (
+  `id` INT NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `email_address` VARCHAR(45) NOT NULL,
+  `phone_number` VARCHAR(45) NOT NULL,
+  `birthdate` DATE NOT NULL,
+  `country` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `roles_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_users_roles_idx` (`roles_id` ASC) VISIBLE,
+  CONSTRAINT `fk_users_roles`
+    FOREIGN KEY (`roles_id`)
+    REFERENCES `app_creator`.`roles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
