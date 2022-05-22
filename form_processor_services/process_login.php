22a803a43e1603c12503c0e376b644ff02f80524
rineb <41300040+rineb@users.noreply.github.com>
2022-05-22 19:17:03 +0200

    process_login/logout: Handled failed login attempts

--- form_processor_services/process_login.php
+++ form_processor_services/process_login.php
@@ -4,6 +4,7 @@ session_start();
 
 require_once("../function_utils/validation.php");
 require_once("../database_services/db_login.php");
+include("../login_error_log/FailedLoginLog.php");
 
 unset($_SESSION["errors"]);
 
@@ -40,11 +41,15 @@ else {
     if($user) {
         $_SESSION["logged_in"] = true;
         $_SESSION["user_id"] = $user["id"];
-        $_SESSION["first_name"]=$user["first_name"];
+        $_SESSION["first_name"] = $user["first_name"];
+        $_SESSION["role_id"] = $user["roles_id"];
         // set to session name + id.
         header("Location: ../index.php");
     } else {
         $errors["login"] = array("Wrong email and password combination!");
+        $writer = new FailedLoginLog();
+        date_default_timezone_set('UTC');
+        $writer->writeToFile($email, date('m/d/Y h:i:s a', time()), $_SERVER['REMOTE_ADDR']);
         manageSession();
         header("Location: ../login.php");
     }
--- form_processor_services/processs_logout.php
+++ form_processor_services/processs_logout.php
@@ -3,6 +3,7 @@ session_start();
 unset($_SESSION["logged_in"]);
 unset($_SESSION["user_id"]);
 unset($_SESSION["first_name"]);
+unset($_SESSION["role_id"]);
 var_dump($_SESSION);
 
 header("Location: ../index.php")
