b219661ea86160f088cc6788ca4d01eb568f12d0
rineb <41300040+rineb@users.noreply.github.com>
2022-05-23 18:17:57 +0200

    Added Pagination to Failed Login Logs

--- failed_login_log.php
+++ failed_login_log.php
@@ -3,7 +3,7 @@ session_start();
 if (!isset($_SESSION["logged_in"]) || (isset($_SESSION["role_id"]) && $_SESSION["role_id"] == "2")) {
     exit("<h1>401 Unauthorized Access</h1>");
 }
-include("login_error_log/FailedLoginLog.php");
+include("login_error_log/PaginationFailedLogin.php");
 ?>
 <!doctype html>
 <html lang="en">
@@ -19,20 +19,39 @@ include("login_error_log/FailedLoginLog.php");
     <h1>Failed Login Attempts Log</h1>
     <div class="content">
         <?php
-            $reader = new FailedLoginLog();
-            try {
-                $failedAttempts = $reader->readFile();
+            if (isset($_GET['records'])) {
+                try {
+                    $reader = new PaginationFailedLogin();
+                    $reader->setNumLines($_GET['records']);
+                    $failedAttempts = $reader->readFile();
 
-                foreach ($failedAttempts as $attempt) {
-                    if (!empty($attempt)) {
-                        echo "<p>$attempt</p>";
+                    foreach ($failedAttempts as $attempt) {
+                        if (!empty($attempt)) {
+                            echo "<p>$attempt</p>";
+                        }
                     }
                 }
-            }
-            catch(Exception $e) {
-                echo $e->getMessage();
+                catch(Exception $e) {
+                    echo $e->getMessage();
+                }
+
+                exit();
             }
         ?>
+
+        <form method="get">
+            <label for="records">Choose number of records:</label>
+
+            <select name="records" id="records">
+                <option value="20">20</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+            </select>
+
+            <br><br>
+            <input type="Submit" value="Submit"/>
+        </form>
+
     </div>
 </body>
 </html>
---
+++ login_error_log/PaginationFailedLogin.php
@@ -0,0 +1,20 @@
+<?php
+include("FailedLoginLog.php");
+    class PaginationFailedLogin extends FailedLoginLog {
+        private $numLines = -2;
+
+        function readFile() {
+            $allRecords = parent::readFile();
+            return array_slice($allRecords, -$this->getNumLines() - 1);
+        }
+
+        public function getNumLines()
+        {
+            return $this->numLines;
+        }
+
+        public function setNumLines($numLines)
+        {
+            $this->numLines = $numLines;
+        }
+    }
